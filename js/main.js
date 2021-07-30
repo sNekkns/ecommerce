7467cf0a7293dd61725299cb1baf12b8cf86cdbb
Julian Orlando <julmotogp2@hotmail.com>
2021-07-30 18:24:05 -0300

    nuevo

--- js/main.js
+++ js/main.js
@@ -1,44 +1,26 @@
+//Animacion al scrollear
 window.addEventListener("scroll", function(){
     let animacion = document.getElementById("grafico");
     let posicionObj1 = animacion.getBoundingClientRect().top;
     let tamañoDePantalla = window.innerHeight;
 
     if(posicionObj1 < tamañoDePantalla){
-        animacion.style.animation = "mover 3s ease-out"
+        animacion.style.animation = "mover 2s ease-out"
     }
 })
 
+//Modo oscuro al presionar categorias
 document.addEventListener("DOMContentLoaded", function(){
-    // make it as accordion for smaller screens
-    if (window.innerWidth < 992) {
-    
-      // close all inner dropdowns when parent is closed
-      document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
-        everydropdown.addEventListener('hidden.bs.dropdown', function () {
-          // after dropdown is hidden, then find all submenus
-            this.querySelectorAll('.submenu').forEach(function(everysubmenu){
-              // hide every submenu as well
-              everysubmenu.style.display = 'none';
-            });
-        })
-      });
-    
-      document.querySelectorAll('.dropdown-menu a').forEach(function(element){
-        element.addEventListener('click', function (e) {
-            let nextEl = this.nextElementSibling;
-            if(nextEl && nextEl.classList.contains('submenu')) {	
-              // prevent opening link if link needs to open dropdown
-              e.preventDefault();
-              if(nextEl.style.display == 'block'){
-                nextEl.style.display = 'none';
-              } else {
-                nextEl.style.display = 'block';
-              }
-    
-            }
-        });
-      })
-    }
-    // end if innerWidth
-    }); 
-    // DOMContentLoaded  end
+  document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+    everydropdown.addEventListener('shown.bs.dropdown', function () {
+        el_overlay = document.createElement('span');
+        el_overlay.className = 'screen-darken';
+        document.body.appendChild(el_overlay)
+    });
+
+    everydropdown.addEventListener('hide.bs.dropdown', function () {
+      document.body.removeChild(document.querySelector('.screen-darken'));
+    });
+  });
+
+}); 
